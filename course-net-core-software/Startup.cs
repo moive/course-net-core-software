09f5cb3b26e6e0b9d2c9e2a957930fd3e36df429
Moises Velasquez <mvelasquez@pictoric.com>
2021-05-01 21:03:47 -0500

    show name process

--- course-net-core-software/Startup.cs
+++ course-net-core-software/Startup.cs
@@ -32,7 +32,7 @@ namespace course_net_core_software
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
                 });
             });
         }
