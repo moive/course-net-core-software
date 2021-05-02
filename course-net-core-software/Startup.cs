b44a2a0073546ab2d9e6cf11de6447ac9380c06f
Moises Velasquez <mvelasquez@pictoric.com>
2021-05-01 21:30:03 -0500

    middleware use

--- course-net-core-software/Startup.cs
+++ course-net-core-software/Startup.cs
@@ -35,25 +35,20 @@ namespace course_net_core_software
 
             app.UseRouting();
 
-            app.Map("/map1", HandleMapTest1);
-            app.Map("/map2", HandleMapTest2);
+            app.Use(async (context, next) => {
+                await context.Response.WriteAsync("way 1 ");
+                await next();
+            });
+
+            app.Use(async (context, next) => {
+                await context.Response.WriteAsync("way 2 ");
+                await next();
+            });
+
             app.Run(async context => {
                 await context.Response.WriteAsync("Hello from non-Map delegate.");
             });
         }
 
-        private static void HandleMapTest1(IApplicationBuilder app)
-        {
-            app.Run(async context => {
-                await context.Response.WriteAsync("Map Test 1");
-            });
-        }
-
-        private static void HandleMapTest2(IApplicationBuilder app)
-        {
-            app.Run(async context => {
-                await context.Response.WriteAsync("Map Test 2");
-            });
-        }
     }
 }
